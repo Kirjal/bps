08969335aeb1d84dd4cf4d16a0e8aba4d9d01cfb
Kirjal <Alexandre_Demontier@hotmail.fr>
2025-03-20 12:22:19 +0100

    bubbles

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Routes from "./routes/Routes";
+import { URL_CONTACT, URL_HOME, URL_SERVICES } from './constants/url';
+
 
 function App() {
+
+  /**bubble logic **************************************************************************/
+  const updateBubbleMode = (value) => {
+    document.getElementById("bubbles").classList.remove("services", "contact");
+    document.getElementById("bubbles").classList.add(value);
+  };
+
+  const bubbles = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99];
+
+  const generateBubblesFront = bubbles.map((height, i) => {
+    return(
+      <div className="bubble bubble_front" style={{top: height + Math.random() * 4 - 2 + "%", left: Math.random() * 125 +  "px"}} key={i}></div>
+    )
+  });
+  const generateBubblesMid = bubbles.map((height, i) => {
+    return(
+      <div className="bubble bubble_mid" style={{top: height + Math.random() * 4 + "%", left: Math.random() * 100 + 25 + "px"}} key={i}></div>
+    )
+  });
+  const generateBubblesBack = bubbles.map((height, i) => {
+    return(
+      <div className="bubble bubble_back" style={{top: height + Math.random() * 4 + 2 + "%", left: Math.random() * 75 + 50 + "px"}} key={i}></div>
+    )
+  })
+  /**********************************************************************************************/
+
+  
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div className={`bubble-container ${"home"}`}>{/** add logic to make the second class dynamic */}
-          <div className="front">{/**NO LOGIC FOR DIV BUBBLES, ADD A GOOD OLD PNG INSTEAD */}</div>
-          <div className="mid"></div>
-          <div className="back"></div>
+    <div className="App">
+      <div className={"bubble-container"} id="bubbles">
+        <div className="front">
+          {generateBubblesFront}
         </div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <div className="title">
-              <h1>Services de plomberie dans la métropole lilloise</h1>
-              <div className="title-gradient"></div>
-          </div>
-          <div className="text">
-            <p>aaaaaaaaaaaaaaaaaaa<br/>aaaaaaa</p>
-          </div>
-          
-        </header>
-        <main>
-          <Routes />
-        </main>
-        <footer></footer>
+        <div className="mid">
+          {generateBubblesMid}
+        </div>
+        <div className="back">
+          {generateBubblesBack}
+        </div>
       </div>
-    </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="title">
+            <h1>Services de plomberie dans la métropole lilloise</h1>
+            <div className="title-gradient"></div>
+        </div>
+        <div className="text">
+          <p>aaaaaaaaaaaaaaaaaaa<br/>aaaaaaa</p>
+        </div>
+          
+      </header>
+      <main>
+        <Routes />
+        <button><Link to={URL_HOME} onClick={()=>updateBubbleMode("home")}>home</Link></button>
+        <button><Link to={URL_SERVICES} onClick={()=>updateBubbleMode("services")}>services</Link></button>
+        <button><Link to={URL_CONTACT} onClick={()=>updateBubbleMode("contact")}>contact</Link></button>
+      </main>
+      <footer></footer>
+    </div>
   );
 }
 
